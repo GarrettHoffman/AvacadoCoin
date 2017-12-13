f63f0409a79ffdeb77a6287456e521148fb20cf4
Garrett Hoffman <garrett.lee.hoffman@gmail.com>
2017-12-12 22:49:07 -0500

    initial CADO commit

---
+++ contracts/Factory.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.15;
+
+import 'contracts/AvacadoCoinIco.sol';
+
+contract Factory {
+
+    function createContract(
+        address _fundsWallet,
+        uint256 _startTimestamp,
+        uint256 _minCapEth,
+        uint256 _maxCapEth) returns(address created) 
+    {
+        return new EspeoTokenIco(
+            _fundsWallet,
+            _startTimestamp,
+            _minCapEth * 1 ether,
+            _maxCapEth * 1 ether
+        );
+    }
+}
